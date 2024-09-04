8bf0e67c49438e27bb9f65787a4a6b6a9a334cd8
grdo4993 <extern.grigorii.dorotynskii@esolutions.de>
2024-09-04 23:00:15 +0200

    // home_page.py: change pattern (locators on the top, find elements in the methods) ++ test_shop.py: add empty cart test

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,38 +1,34 @@
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from tests.conftest import driver
 
 
 class HomePage:
     def __init__(self, driver):
         self.browser = driver
-        # Elements
-        self.page_title_el = self.browser.find_element(By.CLASS_NAME, "page-title")
-        self.cart_button_el = self.browser.find_element(By.CLASS_NAME, "cart-contents")
-        self.empty_message_el = self.browser.find_element(By.CLASS_NAME, "woocommerce-mini-cart__empty-message")
+        # Locators
+        self.page_title_locator = (By.CLASS_NAME, "page-title")
+        self.cart_button_locator = (By.CLASS_NAME, "cart-contents")
+        self.empty_message_locator = (By.CLASS_NAME, "woocommerce-mini-cart__empty-message")
 
     def get_page_title_text(self):
-        return self.page_title_el.text
+        return self.browser.find_element(*self.page_title_locator).text
 
     def click_cart_button(self):
-        return self.cart_button_el.click()
+        return self.browser.find_element(*self.cart_button_locator).click()
 
     def get_amount_of_items_in_cart(self):
-        # return self.cart_button_el.find_element(By.CLASS_NAME, "count").text
-
-        # Find the cart button element every time you interact with it
-        cart_button_el = WebDriverWait(self.browser, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "cart-contents"))
-        )
-        # Find the 'count' element within the cart button element
+        cart_button_el = self.browser.find_element(*self.cart_button_locator)
         count_element = cart_button_el.find_element(By.CLASS_NAME, "count")
         return count_element.text
 
     def hover_over_cart(self):
+        cart_button_el = self.browser.find_element(*self.cart_button_locator)
         actions = ActionChains(self.browser)
-        actions.move_to_element(self.cart_button_el).perform()
+        actions.move_to_element(cart_button_el).perform()
 
-    def get_visibility_of_empty_message(self) :
-        return self.empty_message_el.is_displayed()
+    def get_empty_message_visibility(self):
+        return self.browser.find_element(*self.empty_message_locator).is_displayed()
+
+    def get_empty_message_text(self):
+        return self.browser.find_element(*self.empty_message_locator).text
--- tests/test_shop.py
+++ tests/test_shop.py
@@ -1,3 +1,5 @@
+import time
+
 from tests.conftest import open_home_page
 from pages.home_page import HomePage
 
@@ -6,5 +8,26 @@ def test_shop_title(open_home_page):
     home_page = HomePage(open_home_page)
     actual_page_title_text = home_page.get_page_title_text()
     expected_page_title_text = "Shop"
-    assert actual_page_title_text == expected_page_title_text, (f"Page title should be: {expected_page_title_text}."
-                                                                f" Actual result is: {actual_page_title_text}")
+    assert actual_page_title_text == expected_page_title_text, (
+        f"Page title should be: {expected_page_title_text}."
+        f" Actual result is: {actual_page_title_text}")
+
+
+def test_empty_cart(open_home_page):
+    home_page = HomePage(open_home_page)
+    actual_amount = home_page.get_amount_of_items_in_cart()
+    expected_amount = "0 items"
+    assert actual_amount == expected_amount, (
+        f"Expected amount of items is {expected_amount}."
+        f" Actual result is: {actual_amount}")
+
+    home_page.hover_over_cart()
+    assert home_page.get_empty_message_visibility()
+    actual_empty_message_text = home_page.get_empty_message_text()
+    expected_empty_message_text = "No products in the cart."
+    assert actual_empty_message_text == expected_empty_message_text, (
+        f"Expected empty message text is {expected_empty_message_text}."
+        f" Actual result is: {actual_empty_message_text}")
+
+
+def test_add_item(open_home_page)
